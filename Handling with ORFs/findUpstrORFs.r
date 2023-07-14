a6095644ac7f8c8fdf04bd9a0281fbdc1e01a5dc
Vasily V. Grinev <grinev_vv@bsu.by>
2023-07-14 09:08:42 +0200

    Update findUpstrORFs.r

--- Handling with ORFs/findUpstrORFs.r
+++ Handling with ORFs/findUpstrORFs.r
@@ -1,19 +1,3 @@
-#' Predict the true ORFs in circular RNA molecules
-#' @description Predict the true ORFs in circular RNAs.
-#' @param circRNAs character string giving the name of file with the sequences
-#'     of circular RNAs. This file must be in the working directory. Allowed
-#'     file formats are "fasta" or "fa".
-#' @param orf_length_thr integer value giving the threshold for minimal length
-#'     of ORF(-s). Default value is 42.
-#' @param model character string giving the connection or full path to the file
-#'     from which the classification model is read. Use default NULL value to 
-#'     use default model from system extdata.
-#' @param pr_thr probability threshold for the "winning" class of ORFs.
-#'     The default value is NULL, which means that for a given circular RNA
-#'     molecule, any ORF with the maximum probability value will be selected.
-#' @param workDir character string giving the path to and name of work
-#'     directory. NULL by default that means the current working directory.
-#' @return The coordinates and sequences of ORFs in circular RNAs of interest.
 
 #' @authors Vasily V. Grinev
 #' @examples
@@ -24,7 +8,7 @@
 #'                      prThr=0.9,
 #'                      workDir="D:/Vasily Grinev")
 #' @export
-#' Last updated: June 21, 2023.
+#' Last updated: July 14, 2023.
 
 findUpstrORFs <- function(tr,
                           orfs,
@@ -159,42 +143,3 @@ findUpstrORFs <- function(tr,
     ### Returning the final object.
     return(uORFs)
 }
-
-tr <- "Kasumi-1, transcriptome, StringTie, filtered, all transcripts.fasta"
-orfs <- "Kasumi-1, transcriptome, StringTie, filtered, all transcripts, ORFs.txt"
-res <- findUpstrORFs(tr=tr,
-                     orfs=orfs,
-                     prThr=0.9,
-                     workDir="D:/Vasily Grinev")
-setwd(dir="D:/Vasily Grinev")
-write.table(x=res$uORFs,
-            file="Kasumi-1, transcriptome, StringTie, filtered, all transcripts, uORFs.txt",
-            sep="\t",
-            quote=FALSE,
-            col.names=TRUE,
-            row.names=FALSE)
-prob_orfs <- vectorizeORFs(x=res$seqs)
-model <- readRDS(file=file(description="D:/Vasily Grinev/classRFmodel_1.rds"))
-prob_orfs <- predict(object=model, newdata=prob_orfs, type="prob")
-prob_orfs <- data.table(cbind(res$uORFs, prob=prob_orfs[, "3"]))
-true_orfs <- prob_orfs[prob_orfs[, .I[prob == max(x=prob)],
-                                 by=sequence_id]$V1]
-true_orfs <- true_orfs[true_orfs[, .I[orf_length == max(x=orf_length)],
-                                 by=sequence_id]$V1]
-true_orfs <- true_orfs[true_orfs[, .I[orf_start == min(x=orf_start)],
-                                 by=sequence_id]$V1]
-true_orfs <- data.frame(true_orfs)
-true_orfs <- true_orfs[order(x=true_orfs$sequence_id), ]
-true_orfs1 <- prob_orfs[, c(1:2, 7, 3:6)]
-colnames(x=true_orfs1) <- c("transcript_id",
-                           "orf_id",
-                           "orf_prob",
-                           "orf_start", "orf_end", "orf_length",
-                           "orf_sequence")
-rownames(x=true_orfs1) <- NULL
-write.table(x=true_orfs1,
-            file="uorfs.txt",
-            sep="\t",
-            quote=FALSE,
-            col.names=TRUE,
-            row.names=FALSE)
